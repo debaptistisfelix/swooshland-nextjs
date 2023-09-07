dedf4f17ff091dfc2d6a6d9855341e6bfc87973a
Felix De Baptistis <debaptistisfelix@gmail.com>
2023-09-07 18:40:46 +0200

    changed again next.config

--- next.config.js
+++ next.config.js
@@ -2,24 +2,3 @@
 const nextConfig = {}
 
 module.exports = nextConfig
-// next.config.js
-
-module.exports = {
-    exportPathMap: async function (defaultPathMap, { dev, dir, outDir, distDir, buildId }) {
-      // Exclude pages with server-side rendering enabled
-      const excludedPages = ['/', '/sneakers', "/accessories"];
-  
-      // Filter out the excluded pages
-      const filteredPathMap = Object.keys(defaultPathMap).reduce((pathMap, path) => {
-        if (!excludedPages.includes(path)) {
-          pathMap[path] = defaultPathMap[path];
-        }
-        return pathMap;
-      }, {});
-  
-      return filteredPathMap;
-    },
-  };
-  
-
-
